ef39656208d089e44188affd2b3b814a4861ec1a
gulj <1175327069@qq.com>
2018-04-08 09:57:21 +0800

    add style-loader style-loader/url learn

---
+++ chapter4-1/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+
+module.exports = {
+    entry: {
+        'app': './src/app.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname,'./dist/'),
+        filename: '[name].bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
---
+++ chapter4-2/src/app.js
@@ -0,0 +1,6 @@
+import './css/base.css'
+
+import './css/common.css'
+
+
+
